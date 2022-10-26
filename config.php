90850528834f287b0e456a02d8a3b338650a729f
ivashka <ivanivs@gmail.com>
2022-10-26 10:30:38 +0300

    fix

--- config.php
+++ config.php
@@ -1,27 +1,20 @@
 <?php
 require ('mysqlSwitch.php');
 $config ['host']='localhost';
-$config ['database']='kombat_in_ua';
-$config ['user_datebase']='kombat_in_ua';
+$config ['database']='domajor_new';
+$config ['user_datebase']='domajor_new_usr';
 //mysqldump -ukombat_in_ua -p8qL3sWtlZSlkgdRl kombat_in_ua > 20.09.2022.sql
-$config ['password_datebase']='8qL3sWtlZSlkgdRl';
+$config ['password_datebase']='jgdJSSwe0j8WXJ5E';
 $config ['default_language_admin']='languages/ru.php';
 $config ['default_language_sait']='languages/ru.php';
 $config ['default_language']='ua';
-$config ['default_path'] = '/var/www/kombat_in_ua_usr74/data/www/kombat.in.ua/';
+$config ['default_path'] = '/var/www/domajorcomua/data/www/domajor.com.ua/new/';
 $config ['default_template'] = 'kombat';
 //$config ['default_path'] = '/var/www/ivashka/data/www/dor.net/shop/';
 $config ['time_life_cookie'] = 36000; //время жизни кукиксов в секундах, в том числе и для админа
-if ($_SERVER['HTTP_HOST']=='kombat.in.ua'){
-    $config ['default_template'] = 'kombat';
-    $config ['site_url'] = 'https://kombat.in.ua/';
-//    define('MOBILEVER', 0);
-} else {
-    $config ['default_template'] = 'mbobas';
-    $config ['site_url'] = 'https://m.skombat.in.ua/';
-//    define('MOBILEVER', 1);
-}
-$config['sms_send'] = 'kombat.in.ua';
+$config ['default_template'] = 'domajor';
+$config ['site_url'] = 'https://domajor.com.ua/';
+$config['sms_send'] = 'domajor.com.ua';
 $config ['newSystemSaveItems'] = 1;
 $config['email_admin'] = 'info@picase.com.ua';
 $config['email_password'] = 'Mb2pO0VN';
