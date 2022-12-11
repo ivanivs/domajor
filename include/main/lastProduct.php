ac132080e92d63542c2b23cb2cc5b3799b1651e2
ivashka <ivanivs@gmail.com>
2022-12-11 15:22:06 +0200

    add logo

--- include/main/lastProduct.php
+++ include/main/lastProduct.php
@@ -9,7 +9,7 @@
 $lastProduct = '';
 $htmlTemplate = file_get_contents('templates/'.$config ['default_template'].'/mainOneItem.html');
 if ($arrayItems = getArray("SELECT `ls_items`.* FROM `ls_items` JOIN `ls_values_image` ON `ls_items`.`id` = `ls_values_image`.`id_item` WHERE `ls_items`.`text_4` > 0 AND `ls_values_image`.`id_item` IS NOT NULL GROUP by `ls_items`.`id` ORDER by `ls_items`.`id` DESC LIMIT 0,10")){
-    foreach ($array as $key => $v){
+    foreach ($arrayItems as $key => $v){
         $active = '';
         $oneItem = $htmlTemplate;
         $oneItem = getOneItem($v, $oneItem);
