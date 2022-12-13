6da4f5e992bc86f9e6d6ec5438b67fa850201054
ivashka <ivanivs@gmail.com>
2022-12-13 16:27:50 +0200

    cart

--- include/ajax/getCart.php
+++ include/ajax/getCart.php
@@ -34,20 +34,46 @@ if ($array_item_in_cart = getArray($sql)){
 //        </tr>
 //        ';
         $items .= '
-         <div class="cart_single">
-            <a href="#"><img src="' .getImageFile(getMainImage($info_item['id']), 50, 70, 555). '" alt="" /></a>
-            <h2><a href="'.getItemLink($info_item).'">'.$info_item['text_1'].'</a> <a href="#" onclick="removeItemFromCart('.$v['id'].'); return false;"><span><i class="fa fa-trash"></i></span></a></h2>
-            <p>'.$v['number'].' x '.$info_item['price_1'].' грн.</p>
-        </div>
+            <li>
+                 <div class="tpcart__item">
+                    <div class="tpcart__img">
+                       <img src="{' .getImageFile(getMainImage($info_item['id']), 100, 100, 556). '" alt="">
+                       <div class="tpcart__del">
+                          <a href="#" onclick="removeItemFromCart('.$v['id'].'); return false;"><i class="far fa-times-circle"></i></a>
+                       </div>
+                    </div>
+                    <div class="tpcart__content">
+                       <span class="tpcart__content-title"><a href="'.getItemLink($info_item).'">'.getNameItem($info_item).'</a>
+                       </span>
+                       <div class="tpcart__cart-price">
+                          <span class="quantity">'.$v['number'].' x</span>
+                          <span class="new-price">'.$info_item['price_1'].' грн.</span>
+                       </div>
+                    </div>
+                 </div>
+              </li>
         ';
     }
 //    echo '<a href="'.$config['site_url'].'ua/mode/cart.html" style="color: #FFF;">'.$numberItem.' шт. на суму '.$allPrice.' грн.</a>';
     echo '
-    <a class="list_cl" href="'.$config['site_url'].'ua/mode/cart.html"><i class="fa fa-shopping-cart"></i>Корзина <span class="cart_zero cart_zero1">'.$numberItem.'</span></a>
-    <div class="cart_down_area">
-        '.$items.'
-        <div class="cart_shoptings">
-            <a href="'.$config['site_url'].'ua/mode/cart.html">Оформити замовлення</a>
+    <button class="tpcart__close"><i class="fal fa-times"></i></button>
+    <div class="tpcart">
+    <h4 class="tpcart__title">Ваша корзина</h4>
+         <div class="tpcart__product">
+            <div class="tpcart__product-list">
+                <ul>
+                '.$items.'
+                </ul>
+            </div>
+            <div class="tpcart__checkout">
+               <div class="tpcart__total-price d-flex justify-content-between align-items-center">
+                  <span> Разом:</span>
+                  <span class="heilight-price"> '.$allPrice.' грн.</span>
+               </div>
+               <div class="tpcart__checkout-btn">
+                  <a class="tpcheck-btn" href="'.$config['site_url'].'ua/mode/cart.html">Оформити замовлення</a>
+               </div>
+            </div>
         </div>
     </div>
     ';
@@ -56,7 +82,15 @@ if ($array_item_in_cart = getArray($sql)){
 //    <div class="col-sm-1 col-xs-1" style="line-height: 15px; color: #FFF;">'.$numberItem.' шт. <br>'.$allPrice.' грн.</div>
 //    ';
 } else {
-    echo '<a class="list_cl" href="cart.html"><i class="fa fa-shopping-cart"></i>Корзина <span class="cart_zero cart_zero1">0</span></a>';
+    echo '
+    <button class="tpcart__close"><i class="fal fa-times"></i></button><div class="tpcart">
+    <h4 class="tpcart__title">Ваша корзина</h4>
+         <div class="tpcart__product">
+            <div class="tpcart__product-list">
+                Корзина пуста
+            </div>
+        </div>
+        ';
 }
 /*
  * <button class="tpcart__close"><i class="fal fa-times"></i></button><div class="tpcart">
