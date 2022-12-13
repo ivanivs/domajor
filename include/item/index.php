d83adab96fa301e5c23a92ead7926798c32ae6c8
ivashka <ivanivs@gmail.com>
2022-12-13 09:53:20 +0200

    item page

--- include/item/index.php
+++ include/item/index.php
@@ -1766,6 +1766,7 @@ $body = '
                      <div class="tpproduct-details__pera">
                         <p>Priyoshop has brought to you the Hijab 3 Pieces Combo Pack PS23. It is a <br>completely modern design and you feel comfortable to put on this hijab. <br>Buy it at the best price.</p>
                      </div>
+                     '.(($infoItem['text_4'])>0 ? '
                      <div class="tpproduct-details__count d-flex align-items-center flex-wrap mb-25">
                         <div class="tpproduct-details__quantity">
                            <span class="cart-minus"><i class="far fa-minus"></i></span>
@@ -1773,60 +1774,19 @@ $body = '
                            <span class="cart-plus"><i class="far fa-plus"></i></span>
                         </div>
                         <div class="tpproduct-details__cart ml-20">
-                           <button><i class="fal fa-shopping-cart"></i> Add To Cart</button>
-                        </div>
-                        <div class="tpproduct-details__wishlist ml-20">
-                           <button><i class="fal fa-heart"></i></button>
+                           <button><i class="fal fa-shopping-cart"></i> Додати в корзину</button>
                         </div>
                      </div>
-                     <div class="tpproductdot mb-30">
-                        <a class="tpproductdot__variationitem" href="#">
-                           <div class="tpproductdot__termshape">
-                              <span class="tpproductdot__termshape-bg"></span>
-                              <span class="tpproductdot__termshape-border"></span>
-                           </div>
-                        </a>
-                        <a class="tpproductdot__variationitem" href="#">
-                           <div class="tpproductdot__termshape">
-                              <span class="tpproductdot__termshape-bg red-product-bg"></span>
-                              <span class="tpproductdot__termshape-border red-product-border"></span>
-                           </div>
-                        </a>
-                        <a class="tpproductdot__variationitem" href="#">
-                           <div class="tpproductdot__termshape">
-                              <span class="tpproductdot__termshape-bg orange-product-bg"></span>
-                              <span class="tpproductdot__termshape-border orange-product-border"></span>
-                           </div>
-                        </a>
-                        <a class="tpproductdot__variationitem" href="#">
-                           <div class="tpproductdot__termshape">
-                              <span class="tpproductdot__termshape-bg purple-product-bg"></span>
-                              <span class="tpproductdot__termshape-border purple-product-border"></span>
-                           </div>
-                        </a>
-                     </div>
+                     ' : '').'
+                     
                      <div class="tpproduct-details__information tpproduct-details__code">
-                        <p>SKU:</p><span>BO1D0MX8SJ</span>
+                        <p>SKU:</p><span>'.$infoItem['text_5'].'</span>
                      </div>
                      <div class="tpproduct-details__information tpproduct-details__categories">
                         <p>Categories:</p>
-                        <span><a href="#">T-Shirts,</a></span>
-                        <span><a href="#">Tops,</a></span>
-                        <span><a href="#">Womens</a></span>
-                     </div>
-                     <div class="tpproduct-details__information tpproduct-details__tags">
-                        <p>Tags:</p>
-                        <span><a href="#">fashion,</a></span>
-                        <span><a href="#">t-shirts,</a></span>
-                        <span><a href="#">women</a></span>
-                     </div>
-                     <div class="tpproduct-details__information tpproduct-details__social">
-                        <p>Share:</p>
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-behance"></i></a>
-                        <a href="#"><i class="fab fa-youtube"></i></a>
-                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                        <span><a href="'.getCategoryLink($infoItem['select_1']).'">'.getOneValueText($infoItem['select_1']).',</a></span>
+                        '.((!empty($infoItem['select_2']) AND $infoItem['select_2']!=0) ?? '<span><a href="'.getCategoryLink($infoItem['select_2']).'">'.getOneValueText($infoItem['select_2']).',</a></span>').'
+                        '.((!empty($infoItem['select_3']) AND $infoItem['select_3']!=0) ?? '<span><a href="'.getCategoryLink($infoItem['select_3']).'">'.getOneValueText($infoItem['select_3']).',</a></span>').'
                      </div>
                   </div>
                </div>        
